f1f5d7a4a686e7aec23a1d68539b95f44f20e558
Islam B <blkmlkss@gmail.com>
2026-02-22 18:40:44 +0300

    modified download_lib

--- src/common.rs
+++ src/common.rs
@@ -1,17 +1,25 @@
-use std::{fs, io};
+use anyhow::Context;
 use std::fs::File;
 use std::io::BufWriter;
 use std::path::Path;
-use anyhow::Context;
+use std::{fs, io};
 
-pub fn download_lib_if_needed(lib_dir: impl AsRef<Path>, lib_version: &str) -> anyhow::Result<String> {
+pub fn download_lib_if_needed(
+    lib_dir: impl AsRef<Path>,
+    lib_version: &str,
+) -> anyhow::Result<String> {
     if lib_dir.as_ref().is_file() {
         anyhow::bail!("lib_dir is not a directory");
     }
 
+    #[cfg(target_os = "macos")]
+    let ext = "dylib";
+    #[cfg(target_os = "linux")]
+    let ext = "so";
+
     let lib_file = lib_dir
         .as_ref()
-        .join(format!("libmemtrace_{}.dylib", lib_version));
+        .join(format!("libmemtrace_{}.{ext}", lib_version));
 
     if lib_file.exists() {
         return Ok(lib_file.to_str().unwrap().to_string());
@@ -22,14 +30,14 @@ pub fn download_lib_if_needed(lib_dir: impl AsRef<Path>, lib_version: &str) -> a
     fs::create_dir_all(lib_dir).context("failed to create dirs")?;
 
     let mut response = reqwest::blocking::get(format!(
-        "https://github.com/blkmlk/memtrace-lib/releases/download/{}/libmemtrace_lib.dylib",
+        "https://github.com/blkmlk/memtrace-lib/releases/download/{}/libmemtrace_lib.{ext}",
         lib_version
     ))
-        .context("failed to download libmemtrace.dylib")?;
+    .with_context(|| format!("failed to download libmemtrace_lib.{ext}"))?;
 
     if !response.status().is_success() {
         anyhow::bail!(
-            "failed to download libmemtrace.dylib. status: {}",
+            "failed to download libmemtrace_lib.{ext}. status: {}",
             response.status()
         );
     }
@@ -40,9 +48,10 @@ pub fn download_lib_if_needed(lib_dir: impl AsRef<Path>, lib_version: &str) -> a
     io::copy(&mut response, &mut out_file).context("failed to write output file")?;
 
     println!(
-        "Successfully loaded libmemtrace.dylib version {}",
+        "Successfully loaded libmemtrace_lib.{ext} version {}",
         lib_version
     );
 
     Ok(lib_file.to_str().unwrap().to_string())
 }
+
